ad6d34006e7deffbfdb1df0fc6929c78435241aa
danCraw <dan.craw@yandex.ru>
2022-09-06 23:24:26 +0200

    mobile_vision_fix

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,19 +1,20 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-// eslint-disable-next-line func-names
+function cors (req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://188.225.85.187:8000/api/save_customer');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Accept');
+    next();
+}
+
+module.exports = cors;
+
 module.exports = function (app) {
-    if (process.env.NODE_ENV === 'development') {
-        app.use(
-            '/api',
-            createProxyMiddleware({
-                target: 'http://188.225.85.187:8000/api/save_customer',
-                secure: false,
-                changeOrigin: true,
-                pathRewrite: {
-                    '/': '',
-                },
-            })
-        );
-    }
+
+    app.use('/data',
+        createProxyMiddleware({
+            target: 'http://188.225.85.187:8000/api/save_customer',
+            secure: false,
+            changeOrigin: true,
+        })
+    );
 };
